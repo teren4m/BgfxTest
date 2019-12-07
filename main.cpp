d15aa6d714f04ed76201dbefa99bc35a1bdd96a6
Tim Andriyaschenko <timAndriyaschenko@gmail.com>
2019-12-07 12:02:36 +0200

    Init bgfx

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_syswm.h>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
 
-SDL_Window* window = NULL;
+SDL_Window *window = NULL;
 const int WIDTH = 640;
 const int HEIGHT = 480;
 
-int main(int argc, char* args[])
+int main()
 {
 	printf("Start bgfx sample\n");
 
@@ -28,6 +30,30 @@ int main(int argc, char* args[])
 			       SDL_GetError());
 		}
 	}
+
+	printf("Collect information about the window\n");
+	SDL_SysWMinfo wmi;
+	SDL_VERSION(&wmi.version);
+	if(!SDL_GetWindowWMInfo(window, &wmi)){
+		return 1;
+	}
+
+	bgfx::PlatformData pd;
+	printf("and give the pointer to the window to pd\n");
+	pd.ndt = wmi.info.x11.display;
+	pd.nwh = (void*)(uintptr_t)wmi.info.x11.window;
+
+	printf("Tell bgfx about the platform and window\n");
+	bgfx::setPlatformData(pd);
+	
+	printf("Render empty frame\n");
+	bgfx::renderFrame();
+
+	printf("Initialize bgfx\n");
+	bgfx::init();
+
+	//Reset window
+	bgfx::reset(WIDTH, HEIGHT, BGFX_RESET_VSYNC);
 
 	bool quit = false;
 	SDL_Event currentEvent;
