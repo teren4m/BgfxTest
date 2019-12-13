5a380fc6b028a4a897dd657c488d25752e005973
Tim Andriyaschenko <timAndriyaschenko@gmail.com>
2019-12-13 16:54:51 +0200

    Fix install script

--- install_packages.sh
+++ install_packages.sh
@@ -1,8 +1,4 @@
 
 #!/bin/bash
-
-sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libgl1-mesa-dev x11proto-core-dev libx11-dev
 git submodule update --init --recursive
-cd bgfx
-make linux-debug64 
-mkdir bin
+sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libgl1-mesa-dev x11proto-core-dev libx11-dev
